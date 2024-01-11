97391df6f69bf0294c9f6daca686e17f032da962
Fabio <fabio.cufino@studenti.unipd.it>
2024-01-11 21:51:34 +0100

    Software Grud Search

---
+++ Software_GridSearch/starting_parameters/start_77.cmd
@@ -0,0 +1,5 @@
+
+TROOT::SetMacroPath("/lustre/cmswork/SNN_group/neuron_search/100br/Code");
+.L SNNT13.C
+SNN_Tracking(100000, 1, 10, 10, "/lustre/cmsdata/SNN_group/100k_100br.root", 1, 1e-09, 2.5e-10, 1.68e-09, 1.68e-09, 0.03125, 0.02656, 1, 0, 1, 10, 35, 35);
+.q
