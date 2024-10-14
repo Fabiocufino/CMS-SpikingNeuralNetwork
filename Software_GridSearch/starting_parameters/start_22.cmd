dc2fb6e0238e46a0e4d510f2492d98baff560cf9
Cloud User <centos@snn-genetic.novalocal>
2024-10-14 10:38:26 +0200

    small change

--- Software_GridSearch/starting_parameters/start_22.cmd
+++
@@ -1,1 +0,0 @@
- --N_ev 100000 --N_ep 1 --NL0 15 --NL1 15 --tau_m 1e-09 --tau_s 2.5e-10 --tau_plus 1.68e-09 --tau_minus 1.68e-09 --a_plus 0.03125 --a_minus 0.02656 --CFI0 0.8 --CF01 0.8 --CFI1 0.8 --alpha 10 --TH0 35 --TH1 35 --file_id_GS 22
--- Software_GridSearch/starting_parameters/start_73.cmd
+++
@@ -1,1 +0,0 @@
- --N_ev 100000 --N_ep 1 --NL0 30 --NL1 30 --tau_m 1e-09 --tau_s 2.5e-10 --tau_plus 1.68e-09 --tau_minus 1.68e-09 --a_plus 0.03125 --a_minus 0.02656 --CFI0 0.6 --CF01 0.6 --CFI1 0.6 --alpha 50 --TH0 35 --TH1 35 --file_id_GS 73
